52f9fe28b1e0cfe99e3a37d7cd9c7eb5d803e4bc
ErdelyiPeter <erdelyi.peter04@gmail.com>
2022-05-23 15:52:07 +0200

    Tábla legenerálva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace Sakklepesek_ErdélyiPéter
         public MainWindow()
         {
             InitializeComponent();
+            GombokGeneralasa();
+        }
+
+        private void GombokGeneralasa()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Button gomb = new Button();
+                    gomb.Height = 50;
+                    gomb.Width = 50;
+                    gomb.Margin = new Thickness(50 * j, 50 * i, 0, 0);
+                    hatter.Children.Add(gomb);
+                    if (j % 2 != 0 && i % 2 == 0)
+                    {
+                        gomb.Background = Brushes.Black;
+                    }
+                    else
+                    {
+                        if (j % 2 == 0 && i % 2 != 0)
+                        {
+                            gomb.Background = Brushes.Black;
+                        }
+                        else
+                        {
+                            gomb.Background = Brushes.White;
+                        }
+                    }
+                }
+            }
         }
     }
 }
